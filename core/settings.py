7cc0c44606d761d935f853b6c59c2ee0f84e4998
João Costa <jvcosta1502@gmail.com>
2026-08-24 19:08:20 -0300

    criando model: contact

--- core/settings.py
+++ core/settings.py
@@ -103,7 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/6.1/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "pt-br"
 
 TIME_ZONE = "UTC"
 
@@ -127,3 +127,5 @@ MAILERS = {
         "BACKEND": "django.core.mail.backends.console.EmailBackend",
     },
 }
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
