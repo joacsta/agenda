c1730490de3be96b8a8f900494c64e338504d5e7
Joao Victor Tavares da Costa <e728754@corp.caixa.gov.br>
2026-09-03 13:16:58 -0300

    insere contatos e adiciona estilo no css

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpRequest
+from contact.models import Contact
 
 
 def index(request: HttpRequest):
-    return render(
-        request, 'contact/index.html'
-    )
+    contacts = Contact.objects.all()
+    context = {"contacts": contacts}
+    return render(request, 'contact/index.html', context)
