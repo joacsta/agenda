7dde224e6ed7c4b18304dccd896f5d645d2bd4f3
João Costa <jvcosta1502@gmail.com>
2026-09-03 22:15:58 -0300

    criando partials e fazendo title dinamico

--- contact/admin.py
+++ contact/admin.py
@@ -5,13 +5,23 @@ from contact.models import Category, Contact
 # Register your models here.
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = ("id", "first_name", "last_name", "phone", "show",)
+    list_display = (
+        "id",
+        "first_name",
+        "last_name",
+        "phone",
+        "show",
+    )
     ordering = ("-id",)
     search_fields = ("id", "first_name", "last_name")
     list_filter = ("created_date",)
     list_per_page = 10
     list_max_show_all = 200
-    list_editable = ("first_name", "last_name", "show",)
+    list_editable = (
+        "first_name",
+        "last_name",
+        "show",
+    )
     list_display_links = ("id", "phone")
 
 
--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -6,11 +6,12 @@ from contact.models import Contact
 
 def index(request: HttpRequest):
     contacts = Contact.objects.filter(show=True).order_by("-id")[:31]
-    context = {"contacts": contacts, "title": "Agenda"}
-    return render(request, 'contact/index.html', context)
+    context = {"contacts": contacts, "site_title": "Contatos -"}
+    return render(request, "contact/index.html", context)
 
 
 def single_contact(request: HttpRequest, contact_id: int):
     contact = get_object_or_404(Contact, pk=contact_id, show=True)
-    context = {"contact": contact}
+    site_title = f"{contact.first_name} {contact.last_name} -"
+    context = {"contact": contact, "site_title": site_title}
     return render(request, "contact/single_contact.html", context)
