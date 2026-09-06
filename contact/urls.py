0f08ba45ff881648f68c93d57a4719089010fafc
João Costa <jvcosta1502@gmail.com>
2026-09-06 15:51:30 -0300

    cria sistema de buscas com ordenação

--- contact/urls.py
+++ contact/urls.py
@@ -7,4 +7,5 @@ app_name = "contact"
 urlpatterns = [
     path("", views.index, name="index"),
     path("<int:contact_id>/", views.single_contact, name="single_contact"),
+    path("search/", views.search, name="search"),
 ]
--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.db.models import Q
 from django.http import HttpRequest
+from django.shortcuts import get_object_or_404, redirect, render
 
 from contact.models import Contact
 
@@ -15,3 +16,22 @@ def single_contact(request: HttpRequest, contact_id: int):
     site_title = f"{contact.first_name} {contact.last_name} -"
     context = {"contact": contact, "site_title": site_title}
     return render(request, "contact/single_contact.html", context)
+
+
+def search(request: HttpRequest):
+    search_value: str = request.GET.get("q", "").strip()
+    if not search_value:
+        return redirect("contact:index")
+
+    contacts = (
+        Contact.objects.filter(show=True)
+        .filter(
+            Q(first_name__icontains=search_value)
+            | Q(last_name__icontains=search_value)
+            | Q(phone__icontains=search_value)
+            | Q(email__icontains=search_value)
+        )
+        .order_by("-id")
+    )
+    context = {"contacts": contacts, "site_title": "Procurando - "}
+    return render(request, "contact/index.html", context)
