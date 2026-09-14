8fa1af086a8fd1ce32a85dca188c1295e148c2c2
Joao Victor Tavares da Costa <e728754@corp.caixa.gov.br>
2026-09-14 15:17:09 -0300

    adiciona paginação na landing page.

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.db.models import Q
 from django.http import HttpRequest
 from django.shortcuts import get_object_or_404, redirect, render
@@ -6,8 +7,13 @@ from contact.models import Contact
 
 
 def index(request: HttpRequest):
-    contacts = Contact.objects.filter(show=True).order_by("-id")[:31]
-    context = {"contacts": contacts, "site_title": "Contatos -"}
+    contacts = Contact.objects.filter(show=True).order_by("-id")
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    context = {"page_obj": page_obj, "site_title": "Contatos -"}
+
     return render(request, "contact/index.html", context)
 
 
